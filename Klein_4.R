75f68544b0ce222d04fadef8a71da8b7abf45100
hkbombus <hannaheklein301@gmail.com>
2021-02-16 11:48:28 -0500

    improved graph

--- Klein_4.R
+++ Klein_4.R
@@ -28,22 +28,31 @@ my.pred <- predict(x, type="response")
 #2.) Convert the male/female factor (sex) to be male=0 and female=1. Replot the data.
 my.data$isFemale = as.integer(Sex == "f")
 
-my.log <- glm(isFemale ~ Size, data=my.data, family=binomial("logit"))	
-my.log
-summary(my.log)									# Lots of information here
-
-my.pred1 <- predict(my.log, type="response")				# One of several ways to generate the predicted values
-plot(Size, isFemale)							# Make original plot and ...
-points(Size, isFemale, pch=10, col="blue", cex=0.5)		# Add the predicted as smaller red points
+plot(Size, isFemale, ylab = "Sex: Female (1), Male (0)")							
+points(Size, isFemale, pch=10, col="blue", cex=0.5)		
 
 # 3.) Conduct a logistic regression on the data. Replot the data (from question 2) and make the
 # male and female points different colors and symbols. Add a curve of the predicted relationship
 # between elytral length and sex. Add a legend.
 
+my.log <- glm(isFemale ~ Size, data=my.data, family=binomial("logit"))	
+my.log
+
+my.pred1 <- predict(my.log, type="response")
+
+plot(Size, isFemale, col=as.numeric(isFemale)+33, pch=as.numeric(isFemale)+5,
+     ylab = "Sex: Female (1), Male (0)",
+     xlab = "Lenght",
+     main = "Saperda Tridentata Length by Sex")
+points(Size, my.pred1, pch=16, col="blue", cex=0.8)
+legend(20, .3, legend=c("Female", "Male"), col=c("red", "black"), pch =(6:5)) # cant get legend to work 
 
 # 4.) What is the test statistic, degrees of freedom, and p-value associated with the coefficient for
 # elytral length?
-
+summary(my.log)	
+with(my.log, (null.deviance - deviance))								# Improvement in deviance over null model (just Bo)
+with(my.log, df.null - df.residual)									# Degrees of freedom in the model
+with(my.log, pchisq(null.deviance - deviance, df.null - df.residual, lower.tail = FALSE))
 
 #   5.) What is the significance of the overall model and how did you calculate this?
 
