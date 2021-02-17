21f7e0b703b2970d92b595ed49d43480b9524adc
hkbombus <hannaheklein301@gmail.com>
2021-02-17 18:57:52 -0500

    DONE

--- Klein_4.R
+++ Klein_4.R
@@ -8,7 +8,7 @@
 # library(ROCR)							# ROC curves and other classification evaluation tools
 # library(PredictABEL) 						# Handy for Hosmer-Lemeshow plot for model goodness of fit 
 # library(PresenceAbsence)  					# All kinds of goodies for pres--abs data
-
+#library(ggplot2) 
 
 # 1.) Read in the SaperdaTridentata_elytra.csv data. Plot the data with sex as a predictor of elytral
 # length (size).
@@ -18,6 +18,15 @@ my.data <- read.csv("SaperdaTridentata_elytra.csv", header=T)
 my.data
 attach(my.data)
 names(my.data)
+
+
+sex <-my.data$Sex
+size<-my.data$Size
+df<- data.frame(size, sex)
+plot <-ggplot(df, aes( x=sex,y=size,color=sex)) +
+  geom_point() +
+  labs(title="Length and sex",x="Length", y = "Sex")
+plot + scale_color_brewer(palette ="Set2")+theme_classic()
 
 # not sure how to predict with out making the data binary 
 
@@ -30,8 +39,9 @@ isFemale = as.integer(Sex == "f")
 my.data2 = cbind(my.data,isFemale)
 my.data2
 
-plot(Size, isFemale, ylab = "Sex: Female (1), Male (0)")							
-points(Size, isFemale, pch=10, col="blue", cex=0.5)		
+plot(Size, isFemale, ylab = "Sex: Female (1), Male (0)",
+     pch=10, col="blue", cex=0.8)
+
 
 # 3.) Conduct a logistic regression on the data. Replot the data (from question 2) and make the
 # male and female points different colors and symbols. Add a curve of the predicted relationship
@@ -51,24 +61,28 @@ legend(20, .3, legend=c("Female", "Male"), col=c("red", "black"), pch =(6:5)) # 
 
 # 4.) What is the test statistic, degrees of freedom, and p-value associated with the coefficient for
 # elytral length?  (we want the one on the x variable)
+
 summary(my.log)	
 my.log
-#test statistic =
-#degrees of freedom = 80
-#p-value associated with coefficient = 
+
+#test statistic =  4.318 from the z statistic
+#degrees of freedom = 80 (Null), 79 (Residual)
+#p-value associated with coefficient = 1.58e-05
 
 
 #   5.) What is the significance of the overall model and how did you calculate this?
 
-# sig of intercept and coe, but we dont have the sig of the whole model 
 with(my.log, (null.deviance - deviance))								# Improvement in deviance over null model (just Bo)
 with(my.log, df.null - df.residual)									# Degrees of freedom in the model
 with(my.log, pchisq(null.deviance - deviance, df.null - df.residual, lower.tail = FALSE))
 
+# 4.049984e-15 calculated above
+# Using deviance we calculate the g statistics by comparing the model with and without the prediction to give us 
+# a likelihood number of this pattern occuring randomly. 
+
 #   6.) What does the coefficient tell you about the change in probability of a beetle being male or
 # female for a given length of elytra?
 
-#if this was a simple linear regression, to coe would be slope and that would tell us for the one unit change
-#the y is going to change by this much. In log it means something different. When we go to 17 - 18 what happens
-#to the y. 
-#
+# It is the change in the logit ratio for one change in x. We can see that as we move towards one (female) 0.9436
+# the evidence points to females being more likely to have long antennas.  
+
